40062a427588312d633b91239964477085c07ec9
bofei <bofei@users.noreply.github.com>
2026-07-04 12:02:22 +0800

    ci: 添加 GitHub Actions 构建发布 workflow
    - 从 main2 复制 dotnet.yml workflow (Build, Pack & Publish)
    - 通过 push tag (v*) 触发自动发布到 nuget.org
    - 修复 download_package.sh TFM: net8.0 -> net9.0 (匹配 PDFiumZBindingsGenerator)

--- download_package.sh
+++ download_package.sh
@@ -1,2 +1,2 @@
 dotnet build src/PDFiumZBindingsGenerator/PDFiumZBindingsGenerator.csproj -c Release
-dotnet ./src/PDFiumZBindingsGenerator/bin/Release/net8.0/PDFiumZBindingsGenerator.dll 284405313 false
+dotnet ./src/PDFiumZBindingsGenerator/bin/Release/net9.0/PDFiumZBindingsGenerator.dll 284405313 false
